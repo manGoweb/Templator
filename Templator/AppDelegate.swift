ace7ea3dcc63c93c7c099be32e8820cf771a605b
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2017-09-04 12:30:10 +0100

    Initial Commit

---
+++ Templator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Templator
+//
+//  Created by Ondrej Rafaj on 04/09/2017.
+//  Copyright © 2017 manGoweb UK. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
